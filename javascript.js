c4ec7fcbec45ebef44988d3d070a6d44bb503f4e
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-08-05 14:13:01 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+// ---------------------- js code for smooth scroll 
 var currentPos=0;
 var targetPos=0;
 
@@ -25,4 +25,55 @@ for(var i=0 ; i< anchorTags.length ; i++){
         var coordinates = targetSection.getBoundingClientRect();
         myScroll(0,coordinates.y) ;
     }) ;
+}
+
+// ---------------- js code for auto fill skill section
+
+var skillContainer = document.getElementsByClassName('skills-display');
+var progressBars = document.querySelectorAll('.skill-progress > div') ;
+var animationDone = false;
+window.addEventListener('scroll' , checkScroll) ;
+
+function initBars(){
+    for(let bar of progressBars){
+        bar.style.width = 0+'%' ;
+    }
+}
+
+initBars() ;
+
+function fillBars(){
+
+    for(let bar of progressBars){
+        let targetLevel = bar.getAttribute('data-value') ;
+        let currLevel=0;
+
+        let interval = setInterval(function(){
+
+            if(currLevel > targetLevel){
+                clearInterval(interval) ;
+                return;
+            }else{
+                currLevel++ ;
+                bar.style.width = currLevel + '%' ;
+            }
+
+        },5) ;
+
+    }
+}
+
+
+function checkScroll(){
+
+    var coordinates = skillContainer[0].getBoundingClientRect() ;
+    if(!animationDone && coordinates.top <= window.innerHeight){
+        animationDone=true;
+        fillBars() ;
+        // console.log('visible') ;
+    }else if(coordinates.top > window.innerHeight){
+        animationDone=false;
+        initBars() ;
+    }
+
 }
